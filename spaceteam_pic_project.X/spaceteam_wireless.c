6513771cfb6bedb8408a73a4ea0d49793ed617c2
Dan Pipe-Mazo <dpipemazo@gmail.com>
2014-03-09 23:53:19 -0700

    started working on wireless

--- spaceteam_pic_project.X/spaceteam_wireless.c
+++ spaceteam_pic_project.X/spaceteam_wireless.c
@@ -1,10 +1,24 @@
 // This file contains the functionality used to implement the
 //	wireless communication of the spacteam board
+#include "spi.h"
+#include "xc.h"
+
 
 void init_wireless(void)
 {
-	// At least set the chip select high
+	// Set the chip select high
 	WIRLESS_CS = 1;
+	// Set txrx low
+	WIRELESS_CE = 0;
 
-	
+	// Set up the interrupt to go off of data coming in
+
+	// TODO
+
+}
+
+// This function writes to a register on the 
+void rfid_write_reg(unsigned short reg, unsigned short value)
+{
+
 }
--- spaceteam_pic_project.X/spaceteam_wireless.h
+++ spaceteam_pic_project.X/spaceteam_wireless.h
@@ -3,11 +3,14 @@
 #ifndef SPACETEAM_WIRELESS_H
 #define	SPACETEAM_WIRELESS_H
 
+#include "xc.h"
+
 #ifdef	__cplusplus
 extern "C" {
 #endif
 
 #define WIRELESS_CS LATAbits.LATA7
+#define WIRELESS_CE LATAbits.LATA6
 
 
 #ifdef	__cplusplus
